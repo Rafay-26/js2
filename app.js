b6ea1b689c286873e1a32584aac3a4bdc75b4405
Abdul Rafay <154727770+Rafay-26@users.noreply.github.com>
2024-02-29 11:56:25 +0500

    js assigments

---
+++ app.js
@@ -0,0 +1,138 @@
+      
+
+
+
+// var num1 = +prompt("Enter value 1")
+// var num2 = +prompt("Enter value 2")
+// var op = prompt("Enter operator")
+
+// if(op == "+"){
+//     document.write(num1 + num2)
+// }
+// else if(op == '-'){
+//     document.write(num1 - num2)
+// }
+
+// else if(op == '*'){
+//     document.write(num1 * num2)
+// }
+
+// else if(op == '/'){
+//     document.write(num1 / num2)
+// }
+
+// else if(op == '%'){
+//     document.write(num1 % num2)
+// }
+
+
+
+
+
+
+// var studentName = prompt("Enter Name ")
+// var English = +prompt("Enter English marks")
+// var Maths = +prompt("Enter Maths marks")
+// var Urdu = +prompt("Enter Urdu marks")
+// var Physic = +prompt("Enter Physic marks")
+// var Chemistry = +prompt("Enter Chemistry marks")
+// var Biology = +prompt("Enter Biology marks")
+// var Islamiyat = +prompt("Enter Islamiyat marks")
+// var total = English + Maths + Urdu + Physic + Chemistry  + Biology + Islamiyat 
+// var perc = total / 550 * 100
+// var grade;
+
+// if(perc >= 80){
+//     grade = "A+"
+// }
+// else if(perc >= 70){
+//     grade = "A"
+// }
+
+// else if(perc >= 60){
+//     grade = "B"
+// }
+
+// else if(perc >= 50){
+//     grade = "C"
+// }
+
+// else if(perc >= 40){
+//     grade = "D"
+// }
+// else if (perc < 40){
+//     grade = "F nikama..."
+// }
+
+// document.write(`
+//     <center>
+//     <table border='1px'  width="80%">
+//     <tr>
+//     <th>STUDENT NAME</th>
+//     <th>ENGLISH</th>
+//     <th>MATHS</th>
+//     <th>URDU</th>
+//     <th>PHYSIC</th>
+//     <th>CHEMISTRY</th>
+//     <th>BIOLOGY</th>
+//     <th>ISLAMIYAT</th>
+//     <th>TOTAL</th>
+//     <th>PERCENTAGE</th>
+//     <th>GRADE</th>
+//     </tr>
+
+//      <tr>
+//      <td>${studentName}</td>
+//      <td>${English}</td>
+//      <td>${Maths}</td>
+//      <td>${Urdu}</td>
+//      <td>${Physic}</td>
+//      <td>${Chemistry}</td>
+//      <td>${Biology}</td
+//      ><td>${Islamiyat}
+//      <td>${total} / 550</td>
+//      <td>${perc.toFixed(2)}%</td>
+//      <td>${grade}</td>
+//      </tr>
+//      </table>
+//      </center>
+//      `
+// )
+
+
+// var routerUserName = prompt("Enter email")
+// var routerPassword = prompt("Enter password")
+
+// if(routerUserName == "admin" && routerPassword == "123456"){
+//     alert("login success")
+// }
+// else{
+//     alert("login fail")
+// }
+
+// var routerUserName = prompt("Enter email")
+// var routerPassword = prompt("Enter password")
+
+// if(routerUserName == "admin" || routerPassword == "123456"){
+//     alert("login success")
+// }
+// else{
+//     alert("login fail")
+// }
+
+
+
+
+
+// var num = prompt("Enter number")
+
+// if (num % 2 == 0){
+//     alert("Number is even")
+// }
+// else{
+//     alert("Number is odd")
+// }
+
+
+
+
